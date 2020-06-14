25308507459e8605488756e13d4c25af44b8cdcd
Tara W <tjworrell1@gmail.com>
2020-06-14 15:28:32 +0000

    added new page and attempting to aline titles

---
+++ src/Alt_app.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from  './NewApp.js';
+
+
+ReactDOM.render(
+  <SearchBar />,
+  document.getElementById('root')
+);
--- src/NewApp.js
+++ src/NewApp.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import icon from './location_icon.png';
+import firstH from './firstheading.PNG';
+import secondH from './secondheading.PNG';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -24,13 +26,18 @@ class SearchBar extends React.Component {
   render() {
     return (
     <div className="container">
+        <div className="wordOne">
+            <h3>Get to know your local representatives</h3>
+        </div>
+        <div className="wordTwo">
+            <p>Jpo  We help you discover your local representatives, what their roles are, and how to contact them.</p>
+        </div>
         <div className="buttonHandle">
       <form onSubmit={this.handleSubmit}>
         <label>
           <input type="text" placeholder="Enter your ZIP code" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <button type="button" onClick={this.handleSubmit}>search</button><img src={icon} alt="Location icon"/> 
-         
+        <button type="button" onClick={this.handleSubmit}>search</button><img src={icon} alt="Location icon"/>        
       </form>
       </div>
     </div>
@@ -38,9 +45,5 @@ class SearchBar extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <SearchBar />,
-  document.getElementById('root')
-);
 
 export default SearchBar;
