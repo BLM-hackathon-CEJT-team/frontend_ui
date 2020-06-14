b1260f5e7db1403b5b1637daaa86318254fc2c48
jacquelineturof <jacquelineturof@gmail.com>
2020-06-14 12:12:37 -0400

    updated landing page with logo

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import icon from '../Assets/location_icon.png';
 import firstH from '../Assets/firstheading.png'
 import secondH from '../Assets/secondheading.png';
+import logo from '../Assets/civicSavvyLogo.png';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -26,22 +27,23 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-    <div className="container">
-        <div className="wordOne">
-            <h3>Get to know your local representatives</h3>
-        </div>
-        <div className="wordTwo">
-            <p>Jpo  We help you discover your local representatives, what their roles are, and how to contact them.</p>
-        </div>
-        <div className="buttonHandle">
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input type="text" placeholder="Enter your ZIP code" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <button type="button" onClick={this.handleSubmit}>search</button><img src={icon} alt="Location icon"/>        
-      </form>
+      <div className="container">
+      <img src={logo} alt="Logo"/>
+      <div className="wordOne">
+          <h3>Get to know your local representatives</h3>
       </div>
+      <div className="wordTwo">
+          <p>We help you discover your local representatives, what their roles are, and how to contact them.</p>
+      </div>
+      <div className="buttonHandle">
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        <input type="text" placeholder="Enter your ZIP code" value={this.state.value} onChange={this.handleChange} />
+      </label>
+      <button type="button" onClick={this.handleSubmit}>search</button><img src={icon} alt="Location icon"/>        
+    </form>
     </div>
+  </div>
     );
   }
 }
