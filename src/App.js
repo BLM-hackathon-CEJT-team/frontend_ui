0faed3f33c30d107826fc18b50cc0ab3ccb41875
jacquelineturof <jacquelineturof@gmail.com>
2020-06-14 11:36:55 -0400

    merged code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
 import './icon.config.js'
 
+import Landing from './components/Landing'
 import ResultPage from './components/ResultPage'
 
 function App() {
   return (
     <div className="App">
-      <ResultPage />
+      <Switch>
+        <Route path = "/" exact component = { Landing } />
+        <Route path = "/results" component = { ResultPage } />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
